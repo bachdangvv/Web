5a633aca114694fd8af9b2ba2c03944d1f31b046
bachdangvv <bachdangvv@gmail.com>
2025-08-02 19:39:52 +0700

    up

---
+++ js/demo3.js
@@ -0,0 +1,32 @@
+function f1(){
+    alert("abc");
+}
+
+function f2(){
+    alert("abc");
+}
+
+function f3(){
+    alert("abc");
+}
+
+var ip = document.getElementById("ip");
+var ts = document.getElementById("text-search");
+
+function key1(){
+    alert("key1");
+}
+
+function key2(){
+    alert("key2");
+}
+
+function key3(){
+    var v = ip.value;
+    ts.innerText = v;
+}
+
+function key4(){
+    alert("key4");
+}
+
